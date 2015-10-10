efe7eeca2f902d9a0f072c2b3e156710c7074455
Suhan Park <psh117@gmail.com>
2015-10-10 08:52:04 +0000

    first commit

---
+++ python_script/slm1606m.py
@@ -0,0 +1,91 @@
+import RPi.GPIO as GPIO
+import time
+
+dot_umbr = [
+0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,1,1,1,1,1,0,0,0,0,
+0,0,1,0,0,0,0,0,1,0,0,0,
+0,0,1,0,0,0,1,1,1,1,0,0,
+0,0,0,1,0,1,0,0,1,0,1,0,
+0,0,0,0,1,0,0,0,1,0,1,0,
+0,0,0,0,0,1,0,1,0,0,1,0,
+0,0,0,0,1,1,1,0,0,0,1,0,
+0,1,0,1,1,0,0,1,0,0,1,0,
+0,1,1,1,0,0,0,0,1,1,0,0,
+0,0,1,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0
+]
+
+SEL = 13 # select
+CLK = 19 # clock
+RED = 20 # red 
+GRN = 21 # green
+BRT = 16 # bright
+RST = 26 # reset
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(SEL, GPIO.OUT)
+GPIO.setup(CLK, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(GRN, GPIO.OUT)
+GPIO.setup(BRT, GPIO.OUT)
+GPIO.setup(RST, GPIO.OUT)
+
+GPIO.output(BRT, 0)
+GPIO.output(SEL, 1)
+GPIO.output(RST, 0)
+GPIO.output(RST, 1)
+GPIO.output(RST, 0)
+
+
+def display_dot_1212(dot,duty,redOn,greenOn):
+	for i in range(2):
+                for j in range(16):
+                        GPIO.output(GRN,0)
+			GPIO.output(RED,0)
+                        GPIO.output(CLK,1)
+                        GPIO.output(CLK,0)
+                for j in range(duty):
+                        GPIO.output(BRT,1)
+                GPIO.output(BRT,0)
+
+	for i in range(12):
+		for j in range(2):
+               		GPIO.output(GRN,0)
+                        GPIO.output(RED,0)
+                	GPIO.output(CLK,1)
+                	GPIO.output(CLK,0)
+
+		for j in range(12):
+			GPIO.output(GRN, dot[i*12 + j])
+                        GPIO.output(RED, dot[i*12 + j])
+			GPIO.output(CLK,1)
+			GPIO.output(CLK,0)
+
+		for j in range(2):
+			GPIO.output(GRN,0)
+                        GPIO.output(RED,0)
+                        GPIO.output(CLK,1)
+                        GPIO.output(CLK,0)
+			
+		#GPIO.output(BRT,1)
+		for j in range(duty):
+			GPIO.output(BRT,1)
+		GPIO.output(BRT,0)
+
+        for i in range(2):
+                for j in range(16):
+                        GPIO.output(GRN,0)
+                        GPIO.output(RED,0)
+                        GPIO.output(CLK,1)
+                        GPIO.output(CLK,0)
+                for j in range(duty):
+                        GPIO.output(BRT,1)
+                GPIO.output(BRT,0)
+
+
+
+while(True):
+	display_dot_1212(dot_umbr,200,1,1)
