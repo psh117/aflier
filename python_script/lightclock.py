8a44fab6895475af02e41c97d485213b149df389
Suhan Park <psh117@gmail.com>
2015-10-10 12:55:37 +0000

    Update Main Program

--- python_script/lightclock.py
+++ python_script/lightclock.py
@@ -1,4 +1,5 @@
 from slm1606m import *
+from neopixel_user import *
 from neopixel import *
 
 import threading, time
@@ -18,17 +19,36 @@ class DotThread(threading.Thread):
 	def changeDot(self, dot):
 		self.dot = dot
 
+class LEDThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+                self.__continue = True
+		self.strip.begin()
+	
+	def stop(self):
+		self.__continue = False
 
+	def run(self):
+		while(self.__continue):
+			rainbow(self.strip)
+	
+	
 
 dot_th = DotThread(dot_umbr)
 dot_th.start()
+led_th = LEDThread()
+led_th.start()
 #dot_th.join()
 
 try:
 	while(True):
 		time.sleep(0.1)
+
 except KeyboardInterrupt:
 	dot_th.stop()
+	led_th.stop()
 	print("STOP")
 	dot_th.join()
+	led_th.join()
 
