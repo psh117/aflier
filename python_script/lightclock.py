7612d5239fa26887719c8faa52565b1bfff4f153
Suhan Park <psh117@gmail.com>
2015-10-18 18:32:41 +0900

    Update lightclock.py

--- python_script/lightclock.py
+++ python_script/lightclock.py
@@ -10,22 +10,26 @@ color_blue = Color(0,160,182)
 
 
 #color_
+color_bl = [
+	Color(180,240,250), 
+	Color(110,200,200), 
+	Color(0,80,182), 
+	Color(10,40,220), 
+	Color(0,0,255)
+	]
 
-color_bl_1 = Color(180,240,250)
-color_bl_2 = Color(110,200,200)
-color_bl_3 = Color(0,80,182)
-color_bl_4 = Color(10,40,220)
-color_bl_5 = Color(0,0,255)
 
-
-color_uv_1 = Color(240,220,50)
-color_uv_2 = Color(200,180,0)
-color_uv_3 = Color(182,80,0)
-color_uv_4 = Color(200,40,10)
-color_uv_5 = Color(255,0,0)
+color_rd = [
+	Color(240,220,50),
+	Color(200,180,0),
+	Color(182,80,0),
+	Color(200,40,10),
+	Color(255,0,0)
+	]
 
 color_red = Color(241,15,10)
 color_yellow = Color(182,160,0)
+
 dot = dot_data.dot_17
 class DotThread(threading.Thread):
 	def __init__(self, dot):
@@ -54,25 +58,23 @@ class LEDThread(threading.Thread):
 
 	def run(self):
 		while(self.__continue):
-                        self.strip.setPixelColor(0,color_bl_1)
-                        self.strip.setPixelColor(1,color_bl_1)
-                        self.strip.setPixelColor(2,color_bl_1)
-                        self.strip.setPixelColor(3,color_bl_1)
-                        self.strip.setPixelColor(4,color_bl_1)
-                        self.strip.setPixelColor(5,color_bl_4)
-                        self.strip.setPixelColor(6,color_bl_1)
-                        self.strip.setPixelColor(7,color_bl_1)
-			self.strip.setPixelColor(8,color_bl_2)
-                        self.strip.setPixelColor(9,color_bl_1)
-                        self.strip.setPixelColor(10,color_bl_2)
-                        self.strip.setPixelColor(11,color_bl_4)
+                        self.strip.setPixelColor(0,color_rd[0])
+                        self.strip.setPixelColor(1,color_rd[1])
+                        self.strip.setPixelColor(2,color_rd[1])
+                        self.strip.setPixelColor(3,color_rd[2])
+                        self.strip.setPixelColor(4,color_rd[3])
+                        self.strip.setPixelColor(5,color_rd[3])
+                        self.strip.setPixelColor(6,color_rd[4])
+                        self.strip.setPixelColor(7,color_rd[3])
+			self.strip.setPixelColor(8,color_rd[2])
+                        self.strip.setPixelColor(9,color_rd[1])
+                        self.strip.setPixelColor(10,color_rd[0])
+                        self.strip.setPixelColor(11,color_rd[0])
 
 			#self.strip.setPixelColor(11,Color(0,160,182))
 			self.strip.show()
 			time.sleep(1)
 	
-	
-
 dot_th = DotThread(dot)
 dot_th.start()
 led_th = LEDThread()
