8d87ff806de24319694f60e11a6cbab71fc86592
Suhan Park <psh117@gmail.com>
2015-10-19 01:21:48 +0900

    Update gpiotest.py

--- python_script/gpiotest.py
+++ python_script/gpiotest.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
-import time
+import time, threading
 
 PSD = 23 # select
 SW1 = 27 # clock
 SW2 = 22 # red 
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
@@ -11,24 +12,32 @@ GPIO.setup(PSD, GPIO.IN)
 GPIO.setup(SW1, GPIO.IN)
 GPIO.setup(SW2, GPIO.IN)
 
-sw1_ls = 0 # last status
-sw2_ls = 0
-psd_ls = 0
+class GPIOThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.psd = 0
+		self.sw1 = 0
+		self.sw2 = 0
+        self.__continue = True
+		
+	def stop(self):
+		self.__continue = False
 
+	def run(self):
+	    while (self.__continue):
+            if(self.sw1 != GPIO.input(SW1)):
+                self.sw1 = GPIO.input(SW1)
+                print("SW1 =",self.sw1)
+                time.sleep(0.05)
+            
+            if(self.sw2 != GPIO.input(SW2)):
+                self.sw2 = GPIO.input(SW2)
+                print("SW2 =",self.sw2)
+                time.sleep(0.05)
+            
+            if(self.psd != GPIO.input(PSD)):
+                self.psd = GPIO.input(PSD)
+                print("PSD =",self.psd)
+                time.sleep(0.05)
 
-while (1):
-  if(sw1_ls != GPIO.input(SW1)):
-    sw1_ls = GPIO.input(SW1)
-    print("SW1 =",sw1_ls)
-    time.sleep(0.05)
-    
-  if(sw2_ls != GPIO.input(SW2)):
-    sw2_ls = GPIO.input(SW2)
-    print("SW2 =",sw2_ls)
-    time.sleep(0.05)
-    
-  if(psd_ls != GPIO.input(PSD)):
-    psd_ls = GPIO.input(PSD)
-    print("PSD =",psd_ls)
-    time.sleep(0.05)
-
+	        
