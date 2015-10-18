862ee888274509e5572d59ce443da841af22f63b
Suhan Park <psh117@gmail.com>
2015-10-18 18:35:46 +0900

    Update pc2serial.py

--- python_script/pc2serial.py
+++ python_script/pc2serial.py
@@ -1,42 +1,62 @@
 from raspi_serial import *
 from wifi import Cell, Scheme
 
-gCell = Cell.all('wlan0')
-
 def get_wifi_ssid(cell):
 	cell = Cell.all('wlan0')
 	ssids = [cells.ssid for cells in cell]
 	return ssids
 
-while (1):
-	strs = serial_read_line()
-	print (strs)
-	if(strs == "REQ?CON"):
-		serial_send_line("RSP?CON")
-		print("respond")
-	elif(strs == "GET?SSIDLIST"):
-		ssids = get_wifi_ssid(gCell)
-		serial_send_line("RSP?SSID")
-		serial_send_line(str(len(ssids)))
-		for ssid in ssids:
-			serial_send_line(ssid)
+class PCComThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.__cell = Cell.all('wlan0')
+	        self.__continue = True
 		
-	elif(strs == "SET?SSID"):
-		print("SET SSID")
-		ssid = int(serial_read_line())
-		pswd = serial_read_line()
-		scheme = Scheme.for_cell('wlan0','home',gCell[ssid],pswd)
-		try:
-			scheme.delete()
-		except:
-			pass
-		try:
-			scheme.save()
-		except:
-			pass
-		try:
-			scheme.activate()
-			serial_send_line("RSP?COMP")
-		except:
-			serial_send_line("RSP?SSID")
+	
+	def stop(self):
+		self.__continue = False
+
+	def run(self):
+		while(self.__continue):
+			strs = serial_read_line()
+			print (strs)
+			if(strs == "REQ?CON"):
+				serial_send_line("RSP?CON")
+				print("respond")
+			elif(strs == "GET?SSIDLIST"):
+				ssids = get_wifi_ssid(__cell)
+				serial_send_line("RSP?SSID")
+				serial_send_line(str(len(ssids)))
+				for ssid in ssids:
+					serial_send_line(ssid)
+				
+			elif(strs == "SET?SSID"):
+				print("SET SSID")
+				ssid = int(serial_read_line())
+				pswd = serial_read_line()
+				scheme = Scheme.for_cell('wlan0','home',__cell[ssid],pswd)
+				try:
+					scheme.delete()
+				except:
+					pass
+				try:
+					scheme.save()
+				except:
+					pass
+				try:
+					scheme.activate()
+					serial_send_line("RSP?COMP")
+				except:
+					serial_send_line("RSP?SSID")
     
+if __name__ == '__main__':
+	pc_th = PCComThread()
+	pc_th.start()
+	try:
+		while(True):
+			 time.sleep(0.1)
+	except KeyboardInterrupt: 
+		pc_th.stop()
+		print("STOP")
+		pc_th.joint()
+
